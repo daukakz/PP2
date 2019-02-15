31d4d3598a828adb74adfe30b7ea6e9a803a9a11
daukakz <ddosmuratov@gmaail.com>
2019-02-15 14:09:17 +0600

    first commit

---
+++ Week1/TASK3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine()); 
+
+            int[] a = new int[n];           //Создали массив
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());           //В консоли вводим элементы массива
+            }
+            Console.WriteLine("Array: ");
+            Console.WriteLine(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Two times:");   
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " " + a[i] + " ");         //Дублировали
+            }
+
+        }
+    }
+}
---
+++ Week1/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year_of_study;
+        public void PrintInfo()
+        {
+            Console.Write(name + " " + id + " " + (year_of_study) + " ");
+            Console.WriteLine();
+
+        }
+        public void IncrementYearOfStudy()
+        {
+            year_of_study++;
+            PrintInfo();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.name = Convert.ToString(Console.ReadLine());
+            s.id = Convert.ToString(Console.ReadLine());
+            s.year_of_study = Convert.ToInt32(Console.ReadLine());
+            s.PrintInfo();
+            s.IncrementYearOfStudy();
+        }
+    }
+}
